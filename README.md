9f278928cd4402899b35cfc1ccfdc7e29cdc5c20
pome-ta <pome-ta@users.noreply.github.com>
2021-10-15 00:05:11 +0900

    up

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pysta-opengl
+
+[Objective-CでOpenGL事始め](https://qiita.com/edo_m18/items/6a911da63990d96be7fc)
+
+[GLKit Tutorial for iOS: Getting started with OpenGL ES](https://www.raywenderlich.com/5146-glkit-tutorial-for-ios-getting-started-with-opengl-es)
--- __main__.py
+++ __main__.py
@@ -3,9 +3,6 @@ import ui
 
 import pdbg
 
-#GLKView = ObjCClass('GLKView').alloc()
-#pdbg.state(GLKView)
-
 
 
 class ViewController(ui.View):
@@ -13,27 +10,31 @@ class ViewController(ui.View):
     ui.View.__init__(self, *args, **kwargs)
     self.bg_color = 'slategra'
     self.viewDidLoad()
-    
+
   def viewDidLoad(self):
     f = ((0.0, 0.0), (100.0, 100.0))
     self.glview = ObjCClass('GLKView').alloc()
     self.glview.initWithFrame_(f)
     self.glview.setAutoresizingMask_((1 << 1) | (1 << 4))
-    
+
+    # todo: OpenGL ES 2を使う
+    #EAGLContext = ObjCClass('EAGLContext')
+    #self.glview.context = EAGLContext.alloc().initWithAPI_(2)  # kEAGLRenderingAPIOpenGLES2
     self.glview.context = ObjCClass('EAGLContext').alloc().initWithAPI_(2)  # kEAGLRenderingAPIOpenGLES2
     self.objc_instance.addSubview_(self.glview)
-    EAGLContext = ObjCClass('EAGLContext')
-    EAGLContext.setCurrentContext_(self.glview.context())
+
+    # xxx: delegate `self.glview.delegate = self;`
+
+    # todo: 生成したコンテキストをカレントコンテキストに設定
+    ObjCClass('EAGLContext').setCurrentContext_(self.glview.context())
     self.baseEffect = ObjCClass('GLKBaseEffect').alloc().init()
-    pdbg.state(baseEffect)
+    self.baseEffect.useConstantColor = 1  # GL_TRUE
     
     
-    
-    
-
-
+    pdbg.state(self.baseEffect.constantColor())
 
 
 if __name__ == '__main__':
   vc = ViewController()
   vc.present(style='fullscreen', orientations=['portrait'])
+
