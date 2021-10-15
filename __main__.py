4c04e59590bd570333870aff3909abf2cfb0ed86
pome-ta <pome-ta@users.noreply.github.com>
2021-10-16 02:39:50 +0900

    up

--- __main__.py
+++ __main__.py
@@ -4,7 +4,6 @@ import ui
 import pdbg
 
 
-
 class ViewController(ui.View):
   def __init__(self, *args, **kwargs):
     ui.View.__init__(self, *args, **kwargs)
@@ -12,26 +11,12 @@ class ViewController(ui.View):
     self.viewDidLoad()
 
   def viewDidLoad(self):
-    f = ((0.0, 0.0), (100.0, 100.0))
-    self.glview = ObjCClass('GLKView').alloc()
-    self.glview.initWithFrame_(f)
-    self.glview.setAutoresizingMask_((1 << 1) | (1 << 4))
+    self.setupGL()
 
-    # todo: OpenGL ES 2を使う
-    #EAGLContext = ObjCClass('EAGLContext')
-    #self.glview.context = EAGLContext.alloc().initWithAPI_(2)  # kEAGLRenderingAPIOpenGLES2
-    self.glview.context = ObjCClass('EAGLContext').alloc().initWithAPI_(2)  # kEAGLRenderingAPIOpenGLES2
-    self.objc_instance.addSubview_(self.glview)
-
-    # xxx: delegate `self.glview.delegate = self;`
-
-    # todo: 生成したコンテキストをカレントコンテキストに設定
-    ObjCClass('EAGLContext').setCurrentContext_(self.glview.context())
-    self.baseEffect = ObjCClass('GLKBaseEffect').alloc().init()
-    self.baseEffect.useConstantColor = 1  # GL_TRUE
-    
-    
-    pdbg.state(self.baseEffect.constantColor())
+  def setupGL(self):
+    EAGLContext = ObjCClass('EAGLContext').alloc()
+    context = EAGLContext.initWithAPI_(3)
+    pdbg.state(context.API())
 
 
 if __name__ == '__main__':
